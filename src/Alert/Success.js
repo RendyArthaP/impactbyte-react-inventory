ab26b3f76e09e3a85813af55126a2f3db4f99691
Rendy Artha <rendyartha312@gmail.com>
2021-03-01 23:33:34 +0700

    logic register

---
+++ src/Alert/Success.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Alert} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Success = () => {
+  return (
+    <Alert variant="success">
+      <Alert>Hey, thanks for registration</Alert>
+    </Alert>
+  )
+}
+
+export default Success
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,25 +1,74 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Form, FormControl, FormGroup, FormLabel, Button, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios';
+import Success from '../Alert/Success';
 
 const Register = () => {
+  const history = useHistory()
+  const [userRegister, setUserRegister] = useState({
+    name: "",
+    email: "",
+    password: ""
+  })
+  const [successAlert, setSuccessAlert] = useState(false)
+  const [listUserRegister, setListUserRegister] = useState([])
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setUserRegister({
+      ...userRegister,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log()
+    const url = "https://6023a95a6bf3e6001766b546.mockapi.io/datauser"
+    axios.post(url, {...userRegister})
+      .then(result => {
+        setListUserRegister([...listUserRegister, result.data])
+        setSuccessAlert(true)
+        setTimeout(() => {
+          history.push("/")
+        }, 1000);
+      })
+      .catch(err => console.log(err))
+  }
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Col>
           <h1>Inventory - Form Register</h1>
+          {successAlert && <Success />}
           <FormGroup>
             <FormLabel>Name</FormLabel>
-            <FormControl />
+            <FormControl 
+              type="text" 
+              value={userRegister.name}
+              onChange={handleChange}
+              name="name"
+            />
           </FormGroup>
           <FormGroup>
             <FormLabel>Email Address</FormLabel>
-            <FormControl />
+            <FormControl 
+              type="email"
+              value={userRegister.email}
+              onChange={handleChange}
+              name="email"
+            />
           </FormGroup>
           <FormGroup>
             <FormLabel>Password</FormLabel>
-            <FormControl />
+            <FormControl 
+              type="password" 
+              value={userRegister.password}
+              onChange={handleChange}
+              name="password"
+            />
           </FormGroup>
           <Button variant="primary" type="submit">
             Submit
